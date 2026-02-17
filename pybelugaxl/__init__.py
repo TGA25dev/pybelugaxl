87cce07101f28e5a73cda66ae9c46bf9e35ad043
TGA25dev <letga25@gmail.com>
2026-02-17 21:40:57 +0100

    feat: include get_beluga_fleet_status in module exports

--- pybelugaxl/__init__.py
+++ pybelugaxl/__init__.py
@@ -1,3 +1,3 @@
-from .tracker import get_beluga, is_beluga_in_zone
+from .tracker import get_beluga, is_beluga_in_zone, get_beluga_fleet_status
 
-__all__ = ["get_beluga", "is_beluga_in_zone"]
+__all__ = ["get_beluga", "is_beluga_in_zone", "get_beluga_fleet_status"]
