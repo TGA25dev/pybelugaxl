e176537dc6efd598c8cb829b4f40083bd594de28
TGA25dev <letga25@gmail.com>
2026-03-08 22:21:58 +0100

    feat: update __all__ to include exception classes

--- pybelugaxl/__init__.py
+++ pybelugaxl/__init__.py
@@ -1,5 +1,18 @@
 from .tracker import get_beluga, is_beluga_in_zone
 from .media import get_images
 from .fleet import get_beluga_fleet_status, get_fleet_data
+from .exceptions import InvalidICAOCodeError, InvalidStatusError, InvalidGeoError, InvalidRegistrationError
 
-__all__ = ["get_beluga", "is_beluga_in_zone", "get_beluga_fleet_status", "get_images", "get_fleet_data"]
+__all__ = [
+    #Functions
+    "get_beluga", 
+    "is_beluga_in_zone", 
+    "get_beluga_fleet_status", 
+    "get_images", 
+    "get_fleet_data", 
+    
+    #Exceptions
+    "InvalidICAOCodeError", 
+    "InvalidStatusError", 
+    "InvalidGeoError", 
+    "InvalidRegistrationError"]
